e17efa13ac7df5c48aa41fbe3e6c301554893b0e
Joshua Paup <jopa8541@colorado.edu>
2023-08-16 13:41:20 -0600

    corgi_server.py includes basic Flask functionality

--- corgi_server.py
+++ corgi_server.py
@@ -1,6 +1,10 @@
 from mastodon import Mastodon
 import pandas as pd
 import numpy as np
+from flask import Flask, Response, jsonify
+import json
+
+corgi = Flask(__name__)
 
 # Mastodon.create_app(
 #     'corgi_server',
@@ -10,6 +14,31 @@ import numpy as np
 
 mastodon = Mastodon(api_base_url = 'https://hci.social', access_token = 'corgi_server.secret')
 
-first_twenty = pd.DataFrame(mastodon.timeline(limit=20))
-first_twenty.to_csv("first_twenty.csv")
+# first_twenty = pd.DataFrame(mastodon.timeline(limit=5))
+# first_twenty.to_json("first_twenty.jsond")
 
+@corgi.route('/get_recent_posts')
+def get_recent_posts():
+    first_five = []
+
+    timeline = mastodon.timeline(limit=5)
+
+    for post in timeline:
+        print(post)
+        first_five.append({
+            'username': post.account.username,
+            'content': post.content
+        })
+
+    # This returns a .json-formatted data request
+    # return jsonify(first_five)
+
+    # This returns an easy-to-read .json format of the collected data
+    # json_output = json.dumps(first_five, indent=4)
+    # return Response(json_output, content_type='application/json')
+
+    # Another method for returning an easy-to-read .json format of the collected data
+    return json.dumps(first_five, indent = 4)
+
+if __name__ == '__main__':
+    corgi.run()
